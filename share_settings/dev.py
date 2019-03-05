158a6112ddd6643a5afcdcd04f400a08ecd2a18a
Callum <clpullinger@gmail.com>
2019-03-05 21:56:48 +0000

    Added dev/staging settings files

--- share_settings/dev.py
+++ share_settings/dev.py
@@ -3,6 +3,13 @@ from base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+if DEBUG:
+    from aws_login import AWS_SECRT_ACCESS_KEY as AWS_KEY
+    from aws_login import AWS_ACCESS_KEY_ID as AWS_ID
+
+
+    AWS_ACCESS_KEY_ID = AWS_ID
+    AWS_SECRET_ACCESS_KEY = AWS_KEY
 
 
 # Database
@@ -19,23 +26,16 @@ DATABASES = {
 # Keys
 
 if DEBUG:
-
     from aws_login import *
-
-    from aws_login import AWS_SECRT_ACCESS_KEY as AWS_KEY
-    from aws_login import AWS_ACCESS_KEY_ID as AWS_ID
-
-
-    AWS_ACCESS_KEY_ID = AWS_ID
-    AWS_SECRET_ACCESS_KEY = AWS_KEY
 
 else:
 
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-
+    AWS_ACCESS_KEY_ID = os.getenv('stripe.api_key')
+    AWS_SECRET_ACCESS_KEY = os.getenv('stripe.api_key')
     stripe.api_key = os.getenv('stripe.api_key')
     STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
     STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+
+    # Recaptcha
 
     GOOGLE_RECAPTCHA_SECRET_KEY = os.getenv('GOOGLE_RECAPTCHA_SECRET_KEY')
