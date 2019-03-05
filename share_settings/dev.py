0db06e73706a2068a6689166397754403ac8ac0c
Callum <clpullinger@gmail.com>
2019-03-05 14:26:11 +0000

    Added fundraising targets

--- share_settings/dev.py
+++ share_settings/dev.py
@@ -1,5 +1,11 @@
 from base import *
 
+from aws_login import AWS_ACCESS_KEY_ID as AWS_ID
+from aws_login import AWS_SECRT_ACCESS_KEY as AWS_KEY
+
+AWS_ACCESS_KEY_ID = AWS_ID
+AWS_SECRET_ACCESS_KEY = AWS_KEY
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
