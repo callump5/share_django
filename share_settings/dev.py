42cdd69c38f00d2c35a56cd5c0422aa83a72e8f1
Callum <clpullinger@gmail.com>
2019-03-05 16:07:38 +0000

    Added fundraising targets

--- share_settings/dev.py
+++ share_settings/dev.py
@@ -3,17 +3,6 @@ from base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-if DEBUG:
-    from aws_login import AWS_ACCESS_KEY_ID as AWS_ID
-    from aws_login import AWS_SECRT_ACCESS_KEY as AWS_KEY
-
-    AWS_ACCESS_KEY_ID = AWS_ID
-    AWS_SECRET_ACCESS_KEY = AWS_KEY
-
-else:
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
