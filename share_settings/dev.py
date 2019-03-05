cd924b7ccaf5564761fa44d733da2c6183acd9b7
Callum <clpullinger@gmail.com>
2019-03-05 21:30:05 +0000

    Added dev/staging settings files

--- share_settings/dev.py
+++ share_settings/dev.py
@@ -26,13 +26,16 @@ DATABASES = {
 # Keys
 
 if DEBUG:
+
     from aws_login import *
 
 else:
+
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
     stripe.api_key = os.getenv('stripe.api_key')
     STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
     STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
-
-    # Recaptcha
 
     GOOGLE_RECAPTCHA_SECRET_KEY = os.getenv('GOOGLE_RECAPTCHA_SECRET_KEY')
