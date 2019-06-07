ecbbdab1161cd52072c855ed9dcbc5141344360e
Callum <clpullinger@gmail.com>
2019-06-07 19:37:08 +0100

    Added dev/staging settings files

--- activities/views.py
+++ activities/views.py
@@ -3,17 +3,27 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from home.models import SlideImage, EmailLink, FacebookLink
 from .models import ActivitySheet, ActivityCategory
 
 # Create your views here.
 
 def get_activities(request):
 
+    slides = SlideImage.objects.all()
+    facebook = FacebookLink.objects.all()
+    email = EmailLink.objects.all()
+
     activities = ActivitySheet.objects.all()
     categories = ActivityCategory.objects.all()
 
 
     args = {
+
+        'slides': slides,
+        'facebook': facebook,
+        'email': email,
+
         'activities': activities,
         'categories': categories
     }
